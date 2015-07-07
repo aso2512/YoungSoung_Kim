7efb2aee261fa463f59f3822123451ce8a6c1b26
YoungSoung_Kim <aso2512@naver.com>
2015-07-07 11:18:34 +0900

    Lecture 150707

---
+++ rcode 150707.R
@@ -0,0 +1,16 @@
+
+#Central limit Theorem
+
+hist(runif(10000)*6,main="")
+
+means=numeric(10000)
+for(i in 1:10000)
+{ means[i] <- mean(runif(5)*6) }
+
+hist(means,freq=FALSE)
+
+mean(means)
+sd(means)
+xv=seq(0,10,0.1)
+yv=dnorm(xv,mean=mean(means),sd=sd(means))
+lines(xv,yv)
